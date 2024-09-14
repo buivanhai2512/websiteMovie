4e961fbe72170bcd7ac05431e2c0070281194b9e
haibvph33557 <haibvph33557@fpt.edu.vn>
2024-09-14 21:55:49 +0700

    admin

--- FE/src/router/index.tsx
+++ FE/src/router/index.tsx
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom"
-import LayoutWebsite from "../pages/Client/home/LayoutWebsite"
-import Login from "@/pages/Client/users/_components/Login"
-import Register from "@/pages/Client/users/_components/Resgister"
+import { Route, Routes } from "react-router-dom";
+import LayoutWebsite from "../pages/Client/home/LayoutWebsite";
+import Login from "@/pages/Client/users/_components/Login";
+import Register from "@/pages/Client/users/_components/Resgister";
+import LayoutAdmin from "@/pages/(dashboard)/LayoutAdmin";
 
 const Router = () => {
   return (
     <Routes>
-    <Route path="/" element={<LayoutWebsite/>}>
-    <Route path="/login" element={<Login/>} />
-    <Route path="/register" element={<Register/>} />
-    </Route>
-  </Routes>
-  )
-}
+      <Route path="/" element={<LayoutWebsite />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      <Route path="/admin" element={<LayoutAdmin/>}>
+        <Route index element={<LayoutAdmin/>} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default Router
+export default Router;
